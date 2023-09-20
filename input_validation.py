572226eec95b7d4ae8c14c56f9625c6ace14493f
Coolyer <josh.warburton@yahoo.com>
2023-09-20 03:01:36 +0100

    Removed as not needed

--- input_validation.py
+++
@@ -1,6 +0,0 @@
-def get_integer_input(prompt):
-    while True:
-        value = input(prompt)
-        if value.isdigit():
-            return int(value)
-        print("Invalid input. Please enter a number.")
